1dbe6506d2304dfd9292194a8077362ab05789e2
Jana Schmälzle <jana@familie-schmaelzle.de>
2024-03-22 15:52:29 +0100

    fix ExceptionHandler Bug

--- backend/src/main/java/org/schmaelzle/backend/controller/BookController.java
+++ backend/src/main/java/org/schmaelzle/backend/controller/BookController.java
@@ -4,12 +4,11 @@ package org.schmaelzle.backend.controller;
 import lombok.RequiredArgsConstructor;
 import org.schmaelzle.backend.model.Book;
 import org.schmaelzle.backend.service.BookService;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
+import java.util.NoSuchElementException;
 
 @RestController
 @RequestMapping("/api/books")
@@ -26,5 +25,12 @@ public class BookController {
     @GetMapping("/{id}")
     public Book getBookById(@PathVariable String id){
         return service.getBookById(id);
+    }
+
+
+    @ExceptionHandler(NoSuchElementException.class)
+    @ResponseStatus(HttpStatus.NOT_FOUND)
+    public String handleNoSuchElementException(NoSuchElementException e) {
+        return e.getMessage();
     }
 }
