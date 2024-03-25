6ccf89e4a29fb61ed0b2d5b88996950dedc4a478
Jana Schmälzle <jana@familie-schmaelzle.de>
2024-03-25 15:28:22 +0100

    update clickable Book Card for keyboard user

--- frontend/src/components/BookCard/BookCard.tsx
+++ frontend/src/components/BookCard/BookCard.tsx
@@ -12,8 +12,17 @@ export default function BookCard(props: Readonly<BookCardProps>) {
         navigate("/books/" + props.book.id)
     }
 
+    function handleKeyDown(event: React.KeyboardEvent) {
+        if(event.key === "Enter") {
+            navigateToDetailPage()
+        }
+    }
+
     return (
-        <div className={"BookCard"} onClick={navigateToDetailPage} >
+        <div className={"BookCard"}
+             onClick={navigateToDetailPage}
+             onKeyDown={handleKeyDown}
+             tabIndex={0}>
             <h2 className={"BookTitle"}>{props.book.title}</h2>
             <h3 className={"BookAuthor"}>{props.book.author}</h3>
         </div>
