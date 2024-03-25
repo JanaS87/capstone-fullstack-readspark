7ddf6fa05678e125b453973c78a114baae5cc6a8
Jana Schmälzle <jana@familie-schmaelzle.de>
2024-03-25 15:35:47 +0100

    update book cards

--- frontend/src/components/BookCard/BookCard.tsx
+++ frontend/src/components/BookCard/BookCard.tsx
@@ -1,30 +1,19 @@
 import {Book} from "../../types/Book.ts";
-import {useNavigate} from "react-router-dom";
+import {Link} from "react-router-dom";
 
 type BookCardProps = {
     book: Book,
 
 }
 export default function BookCard(props: Readonly<BookCardProps>) {
-    const navigate = useNavigate()
 
-    function navigateToDetailPage() {
-        navigate("/books/" + props.book.id)
-    }
-
-    function handleKeyDown(event: React.KeyboardEvent) {
-        if(event.key === "Enter") {
-            navigateToDetailPage()
-        }
-    }
 
     return (
-        <div className={"BookCard"}
-             onClick={navigateToDetailPage}
-             onKeyDown={handleKeyDown}
-             tabIndex={0}>
+        <>
+            <Link to={"/books/" + props.book.id} className={"BookCard"}>
             <h2 className={"BookTitle"}>{props.book.title}</h2>
             <h3 className={"BookAuthor"}>{props.book.author}</h3>
-        </div>
+            </Link>
+        </>
     )
 }
