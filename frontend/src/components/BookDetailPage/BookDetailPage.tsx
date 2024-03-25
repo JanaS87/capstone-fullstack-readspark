d0171b79992f1f5916d21beecb52c56e4fedef59
Jana Schmälzle <jana@familie-schmaelzle.de>
2024-03-25 15:13:34 +0100

    add routes, implement google books api for book cover for DetailPage

--- frontend/src/components/BookDetailPage/BookDetailPage.tsx
+++ frontend/src/components/BookDetailPage/BookDetailPage.tsx
@@ -1,12 +1,55 @@
 import {Book} from "../../types/Book.ts";
+import {useEffect, useState} from "react";
+import axios from "axios";
+import {Link, useParams} from "react-router-dom";
 
-type BookDetailPageProps = {
-    books: Book[],
-}
-export default function BookDetailPage(props: Readonly<BookDetailPageProps>) {
+export default function BookDetailPage() {
+    const {id} = useParams<{id: string}>();
+    const [book, setBook] = useState<Book | null>(null)
+    const [bookImage, setBookImage] = useState<string | null>(null)
+
+    useEffect(() => {
+        axios.get(`/api/books/${id}`)
+            .then(response => {
+                setBook(response.data);
+            })
+            .catch(error => {
+                console.error('Error fetching Book: ', error);
+                console.error('Error Details: ', error.response);
+            });
+    }, [id]);
+
+    useEffect(() => {
+        if (book) {
+            axios.get(`https://www.googleapis.com/books/v1/volumes?q=isbn:${book.isbn}`)
+                .then(response => {
+                   const data = response.data;
+                   if (data.items && data.items[0].volumeInfo.imageLinks && data.items[0].volumeInfo.imageLinks.thumbnail) {
+                       setBookImage(data.items[0].volumeInfo.imageLinks.thumbnail);
+                   }
+                })
+                .catch(error => {
+                    console.error('Error fetching Book Image: ', error);
+                    console.error('Error Details: ', error.response);
+                });
+        }
+    }, [book]);
+
+
     return (
         <div>
             <h1>Book Detail Page</h1>
+            <Link to={'/'}>Übersicht</Link>
+            {book && (
+                <div>
+                    {bookImage && <img src={bookImage} alt={"Buchcover"}/>}
+                    <h2>{book.title}</h2>
+                    <h3>{book.author}</h3>
+                    <p>{book.publisher}</p>
+                    <p>{book.genre}</p>
+                    <p>{book.blurb}</p>
+                </div>
+            )}
         </div>
     )
 }
