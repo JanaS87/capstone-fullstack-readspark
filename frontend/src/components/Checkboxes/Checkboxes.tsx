95cc0829fce24a4a0713e6c340b390b2db71213b
Jana Schmälzle <jana@familie-schmaelzle.de>
2024-03-30 14:39:55 +0100

    create checkbox component

---
+++ frontend/src/components/Checkboxes/Checkboxes.tsx
@@ -0,0 +1,33 @@
+import Checkbox from "@mui/material/Checkbox";
+import FormControlLabel from "@mui/material/FormControlLabel";
+import FormGroup from "@mui/material/FormGroup";
+import React from "react";
+
+type CheckBoxProps = {
+    checked: boolean,
+    onChange: (event: React.ChangeEvent<HTMLInputElement>) => void,
+    label: string
+}
+
+export default function Checkboxes({checked, onChange, label}: CheckBoxProps) {
+    return (
+        <FormGroup>
+            <FormControlLabel required control={
+                <Checkbox
+                    checked={checked}
+                    onChange={onChange}
+                />
+            }
+                              label={label}
+            />
+            <FormControlLabel required control={
+                <Checkbox
+                    checked={checked}
+                    onChange={onChange}
+                />
+            }
+                              label={label}
+            />
+        </FormGroup>
+    )
+}
