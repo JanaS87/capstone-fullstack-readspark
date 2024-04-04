dda66df9c706d2e64ad5868019c02b074fbe0811
Jana Schmälzle <jana@familie-schmaelzle.de>
2024-04-04 15:19:54 +0200

    create Mapping for GoogleBookController, create Test for GoogleBookController, update GoogleBookService, deleted useless GoogleBookServiceTest

--- backend/src/main/java/org/schmaelzle/backend/google/GoogleBookService.java
+++ backend/src/main/java/org/schmaelzle/backend/google/GoogleBookService.java
@@ -1,11 +1,15 @@
 package org.schmaelzle.backend.google;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
 import org.springframework.web.client.RestClient;
 @Service
 public class GoogleBookService {
 
-    private final RestClient restClient = RestClient.builder().baseUrl("https://www.googleapis.com/books/v1/volumes").build();
+    private final RestClient restClient;
+    public GoogleBookService( @Value("${google.books.api.url}")String googleBooksUrl) {
+        this.restClient = RestClient.builder().baseUrl(googleBooksUrl).build();
+    }
 
     public GoogleBookResponse searchBooks(String query) {
         return restClient.get()
@@ -18,5 +22,17 @@ public class GoogleBookService {
                 .toEntity(GoogleBookResponse.class)
                 .getBody();
 
+    }
+
+    public GoogleBookResponse searchBooksByBarcode(String barcode) {
+        return restClient.get()
+                .uri(uriBuilder -> uriBuilder
+                        .queryParam("q", "isbn:" + barcode)
+                        .queryParam("langRestrict","de")
+                        .queryParam("printType","books")
+                        .build())
+                .retrieve()
+                .toEntity(GoogleBookResponse.class)
+                .getBody();
     }
 }
