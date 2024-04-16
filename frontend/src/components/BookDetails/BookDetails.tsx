441e7e063e6aa2a2e084ec214c06b579ccedcd6b
Jana Schmälzle <jana@familie-schmaelzle.de>
2024-04-16 15:18:35 +0200

    fix visibility for book details

--- frontend/src/components/BookDetails/BookDetails.tsx
+++ frontend/src/components/BookDetails/BookDetails.tsx
@@ -16,31 +16,33 @@ export default function BookDetails({selectedBook}: Readonly<BookDetailsProps>) 
     }
 
     return (
+        <div className={"blurry-background"}>
         <div className={"selected-book-wrapper"}>
-            <img className={"book-img"} src={selectedBook.volumeInfo.imageLinks?.thumbnail}
-                 alt={selectedBook.volumeInfo.title}/>
-            <div className={"information-wrapper"}>
-                <p className={"information-title"}><span>Titel:</span> {selectedBook.volumeInfo.title}</p>
+                <img className={"book-img"} src={selectedBook.volumeInfo.imageLinks?.thumbnail}
+                     alt={selectedBook.volumeInfo.title}/>
+                <div className={"information-wrapper"}>
+                    <p className={"information-title"}><span>Titel:</span> {selectedBook.volumeInfo.title}</p>
                 <p className={"information-author"}><span>Autor:</span> {selectedBook.volumeInfo.authors?.join(", ")}</p>
-                <p className={"information-publisher"}><span>Verlag:</span> {selectedBook.volumeInfo.publisher}</p>
+                    <p className={"information-publisher"}><span>Verlag:</span> {selectedBook.volumeInfo.publisher}</p>
                 <p className={"information-category"}><span>Genre:</span> {selectedBook.volumeInfo.categories?.join(", ")}</p>
-                <p className={"description-text"}>
-                    <span>Beschreibung:</span>
-                    {isExpanded ? selectedBook.volumeInfo.description
-                        :
-                        (selectedBook.volumeInfo.description?.substring(0, 200) + '...')}
-                    <Button aria-label={"expand text"}
-                            variant={"contained"}
-                            onClick={handleToggleExpand}
-                            className={"expand-button"}
-                            style={{backgroundColor: "#423F3E", color: "white"}}
-                            size={"small"}
+                    <p className={"description-text"}>
+                        <span>Beschreibung:</span>
+                        {isExpanded ? selectedBook.volumeInfo.description
+                            :
+                            (selectedBook.volumeInfo.description?.substring(0, 200) + '...')}
+                        <Button aria-label={"expand text"}
+                                variant={"contained"}
+                                onClick={handleToggleExpand}
+                                className={"expand-button"}
+                                style={{backgroundColor: "#423F3E", color: "white"}}
+                                size={"small"}
 
-                    >
-                        {isExpanded ? 'Weniger anzeigen' : 'Mehr anzeigen'}
-                    </Button>
-                </p>
-            </div>
+                        >
+                            {isExpanded ? 'Weniger anzeigen' : 'Mehr anzeigen'}
+                        </Button>
+                    </p>
+                </div>
+        </div>
         </div>
     )
 }
